e70f3b63048aee8304ebaeee9f34f828e371e954
rojasarahardik <hardik.rojasara1412@gmail.com>
2026-05-29 12:13:46 +0530

    Added Week 1 Python programs and summary

--- week_1.py
+++ week_1.py
@@ -1,3 +1,4 @@
+
 #client Project :- Create a basic data processing script
 
 # Average Temperature Calculator
@@ -13,3 +14,18 @@ for i in range(days):
 average = total_temperature / days
 
 print("Average Temperature is:", average)
+
+
+"""
+summary :-
+
+In Week 1, I learned the fundamentals of Python programming. 
+I studied variables, data types, operators, input/output statements, conditional statements (if-else), and loops (for and while).
+I created basic Python programs such as:
+Temperature Converter
+Simple Calculator
+I also worked on a basic data processing project to calculate the average temperature using user input and loops.
+Through these tasks, I improved my understanding of Python syntax, logical thinking, and problem-solving skills.
+ I also learned how to take user input, perform calculations, and display outputs effectively.
+
+"""
